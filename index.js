ecbed3655109f075a709b5cde3fe08e7d354c7cd
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2023-12-09 23:51:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,23 @@
-/* Copyright (C) 2020 Yusuf Usta.
+const { Client, logger } = require('./lib/client')
+const { DATABASE, VERSION } = require('./config')
+const { stopInstance } = require('./lib/pm2')
 
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-*/
-
-const chalk = require('chalk');
-const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
-const {StringSession} = require('./julie/');
-const fs = require('fs');
-
-async function whatsAsena () {
-    const conn = new WAConnection();
-    const Session = new StringSession();  
-    conn.version = [3, 3234, 9]
-    conn.logger.level = 'warn';
-    conn.regenerateQRIntervalMs = 50000;
-    
-    conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
-${chalk.white.italic('AsenaString Kodu Alıcı')}
-
-${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
-    });
-    
-
-    conn.on('open', async () => {
-        var st = Session.createStringSession(conn.base64EncodedAuthInfo());
-        console.log(
-            chalk.green.bold('Asena String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
-        );
-        
-        if (!fs.existsSync('config.env')) {
-            fs.writeFileSync('config.env', `ASENA_SESSION="${st}"`);
-        }
-        if (conn.user.jid.startsWith('90')) {
-            await conn.sendMessage(conn.user.jid,st, MessageType.text)
-            await conn.sendMessage(conn.user.jid,'*Bu Kodu Kimseyle Paylaşmayın!*', MessageType.text)
-            console.log(
-                chalk.blue.bold('Locale kuruyorsanız node bot.js ile botu başlatabilirsiniz.')
-            );
-        }
-        else {
-            await conn.sendMessage(conn.user.jid,st, MessageType.text)
-            await conn.sendMessage(conn.user.jid,'*Do Not Share This Code With Anyone!*', MessageType.text)
-            console.log(
-                chalk.blue.bold('If you are installing locale, you can start the bot with node bot.js')
-            );
-        }
-        
-        process.exit(0);
-    });
-
-    await conn.connect();
+const start = async () => {
+  logger.info(`levanter ${VERSION}`)
+  try {
+    await DATABASE.authenticate({ retry: { max: 3 } })
+  } catch (error) {
+    console.error('Unable to connect to the database:', error.message, process.env.DATABASE_URL)
+    return stopInstance()
+  }
+  try {
+    logger.info('Database syncing...')
+    await DATABASE.sync()
+    const bot = new Client()
+    await bot.init()
+    await bot.connect()
+  } catch (error) {
+    logger.error(error)
+  }
 }
-
-whatsAsena()
+start()
